ce4fa280b17778165601ace6baca94fd16556f96
Nicholas Offer <noffer@live.com>
2020-04-19 18:20:10 -0400

    WIP bookmarklet

--- sync_start.bookmarklet.js
+++ sync_start.bookmarklet.js
@@ -1,3 +1,34 @@
 (function (exports) {
   let sock = new WebSocket("wss://45.62.212.114:8765");
+
+  let play_video = () => {
+    document.querySelector("video").play();
+  };
+  let pause_video = () => {
+    document.querySelector("video").pause();
+  };
+
+  sock.onclose = () => alert("Video Sync is disconnected");
+  sock.onopen = () => alert("Video Sync is connected");
+  sock.onmessage = (m) => {
+    let msg = m.data;
+    if (msg === "play") {
+      play_video();
+    } else if (msg === "pause") {
+      pause_video();
+    }
+  };
+
+  let sync_play = () => {
+    sock.send("play");
+  };
+  let sync_pause = () => {
+    sock.send("pause");
+  };
+  let sync_disconnect = () => {
+    sock.close();
+  };
+  exports.sync_play = sync_play;
+  exports.sync_pause = sync_pause;
+  exports.sync_disconnect = sync_disconnect;
 })(window);
